d0621a60d68db7967b294832f92ce38ec6ce0d40
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-05-25 20:12:07 +0200

    NewsHubViewController (feat): add method call to populate table view cell.

--- NewsApp/Application/ViewController/NewsHub/NewsHubViewController.m
+++ NewsApp/Application/ViewController/NewsHub/NewsHubViewController.m
@@ -10,7 +10,7 @@
 
 @interface NewsHubViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (strong, nonatomic) UITableView* tableView;
-@property (strong, nonatomic) NSArray* content;
+@property (nonatomic) NSMutableArray* news;
 @end
 
 @implementation NewsHubViewController
@@ -18,7 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.content = @[@"Monday", @"Tuesday", @"Wednesday"];
+    self.news = [[NSMutableArray alloc] init];
+    
+    [self populateNewsArray];
     
     self.title = @"";
     
@@ -26,32 +28,52 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     
-    [self.tableView registerClass:NewsCell.class forCellReuseIdentifier:NewsCell.identifier];
+    [self.tableView registerClass: NewsCell.class forCellReuseIdentifier: NewsCell.identifier];
     
     [self.view addSubview:self.tableView];
     
 }
 
+- (void)populateNewsArray {
+    NewsModel model1;
+    model1.title = @"Title";
+    model1.author = @"Author";
+    model1.description = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id aliquet risus. Ut mollis nibh velit, in consectetur tellus molestie ac. Cras tincidunt enim ligula, elementum tempus purus mollis nec. Nunc eleifend ultrices dignissim. Vivamus sed augue mattis, lacinia ante blandit, euismod ante.";
+    model1.banner = nil;
+    NSValue *boxedModel1 = [NSValue valueWithBytes:&model1 objCType:@encode(NewsModel)];
+    NSValue *boxedModel2 = [NSValue valueWithBytes:&model1 objCType:@encode(NewsModel)];
+    NSValue *boxedModel3 = [NSValue valueWithBytes:&model1 objCType:@encode(NewsModel)];
+    
+    [self.news addObject:boxedModel1];
+    [self.news addObject:boxedModel2];
+    [self.news addObject:boxedModel3];
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return _content.count;
+    return [self.news count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    NewsCell* cell = [self.tableView dequeueReusableCellWithIdentifier:NewsCell.identifier forIndexPath:indexPath];
+    NewsCell *cell = [self.tableView dequeueReusableCellWithIdentifier:NewsCell.identifier forIndexPath:indexPath];
+    
+    NewsModel tmpModel;
+    NSValue *retVal = [self.news objectAtIndex:indexPath.row];
+    [retVal getValue:&tmpModel];
     
     if (cell == nil) {
         UITableViewCell* defaultCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"defaultCell"];
-        defaultCell.textLabel.text = [_content objectAtIndex:indexPath.row];
+        defaultCell.textLabel.text = tmpModel.title;
+        defaultCell.detailTextLabel.text = tmpModel.description;
         return defaultCell;
     }
     
-    cell.textLabel.text = [_content objectAtIndex:indexPath.row];
+    [cell setupWithNews:tmpModel];
     
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSLog(@"Did click on cell: %@", [_content objectAtIndex:indexPath.row]);
+    NSLog(@"Did click on cell: %@", [self.news objectAtIndex:indexPath.row]);
 }
 
 @end
