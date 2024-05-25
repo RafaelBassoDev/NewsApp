e9805b8ed8282f699bdeea963fda37927b1ae83b
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-05-25 20:07:13 +0200

    NewsHub (feat): create 'NewsModel.h'.

---
+++ NewsApp/Application/ViewController/NewsHub/NewsCell/NewsModel.h
@@ -0,0 +1,15 @@
+//
+//  News.h
+//  NewsApp
+//
+//  Created by Rafael Basso on 22/05/24.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef struct {
+    NSString* title;
+    NSString* author;
+    NSString* description;
+    UIImage* banner;
+} NewsModel;
